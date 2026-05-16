61065101412f2d992c48f251e1b78c51097e1ae6
benja1216 <benjammondragon@gmail.com>
2026-05-15 23:50:08 -0600

    modificación para el Frontend

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from dotenv import load_dotenv
 
@@ -18,6 +19,13 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials = True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 class Consulta(BaseModel):
     pregunta: str
